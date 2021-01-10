c456a4266c04ae18ec851e387a7c54badd19530e
Nilo Almeida <niloalmeidadev@gmail.com>
2021-01-10 18:46:38 -0300

    melhorando teste de retornar usuários

--- src/__tests__/user.spec.js
+++ src/__tests__/user.spec.js
@@ -2,11 +2,17 @@ var should = require("should");
 var request = require("request");
 var chai = require("chai");
 var expect = chai.expect;
+var assert = chai.assert;
 var urlBase = "http://localhost:3001";
 
 it("Deve retornar usuários do banco de dados", function(done){
     request.get({ url : urlBase + "/users"}, (error, response, body)=>{
-        expect(response.statusCode).to.equal(200);
+				const _body = JSON.parse(body);		
+        if(expect(response.statusCode).to.equal(200)){
+					assert.isArray(_body);
+					assert.isObject(_body[0]);
+					assert.property(_body[0], 'username');
+				}
 				done();
       }
     );
